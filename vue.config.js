0b0311e3777798bf12c5cb4e0e6d6d2b55c0a00b
Sergey <serega-nsk93@yandex.ru>
2021-07-30 13:40:47 +0300

    Fix config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'prod'
         ? '/dashboard/'
-        : '/'
+        : '/',
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            title: 'Dashboard',
+        }
+    }
 }
